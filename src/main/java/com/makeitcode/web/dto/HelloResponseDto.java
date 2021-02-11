d9be9cf96fbaf76de496de9c3c36ec26fb67e4f2
slamdunk7575 <slamdunk7575@gmail.com>
2021-02-11 22:37:25 +0900

    (feat) Dto 생성 및 테스트

---
+++ src/main/java/com/makeitcode/web/dto/HelloResponseDto.java
@@ -0,0 +1,19 @@
+package com.makeitcode.web.dto;
+
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+    public HelloResponseDto(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
--- src/test/java/com/makeitcode/web/HelloControllerTest.java
+++ src/test/java/com/makeitcode/web/HelloControllerTest.java
@@ -1,14 +1,15 @@
 package com.makeitcode.web;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @ExtendWith(SpringExtension.class)
@@ -19,7 +20,8 @@ class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws Exception {
+    @DisplayName("hello_리턴_테스트")
+    public void helloReturnTest() throws Exception {
         // given
         String hello = "hello";
 
@@ -27,6 +29,22 @@ class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    @DisplayName("helloDto_리턴_테스트")
+    public void helloDtoReturnTest() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+
+        // when & then
+        mvc.perform(get("/hello/dto")
+        .param("name", name)
+        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.amount").value(amount));
     }
 
 }
