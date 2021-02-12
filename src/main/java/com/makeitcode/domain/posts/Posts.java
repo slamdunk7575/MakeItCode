dce4673f01dd4fac16544208364f5b7dfc2773e8
slamdunk7575 <slamdunk7575@gmail.com>
2021-02-12 23:55:45 +0900

    (feat) 게시글(Posts) 도메인 구현 및 테스트

---
+++ src/main/java/com/makeitcode/domain/posts/Posts.java
@@ -0,0 +1,43 @@
+package com.makeitcode.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ src/test/java/com/makeitcode/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.makeitcode.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("게시글_저장_불러오기")
+    public void findPostsTest() {
+        // given
+        String title = "스터디 모집";
+        String content = "자바 스터디 모집합니다.";
+        String author = "slamdunk7575@gmail.com";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getAuthor()).isEqualTo(author);
+    }
+
+}
